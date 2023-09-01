6e2d687ea6c9980a8eeb80679c5a4b4119a49fe7
ray-jpl <72333601+ray-jpl@users.noreply.github.com>
2023-09-02 00:12:37 +1000

    Extend colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,15 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'text': 'hsl(197, 78%, 7%)',
+        'background': 'hsl(195, 80%, 98%)',
+        'primary': 'hsl(17, 79%, 68%)',
+        'secondary': 'hsl(47, 80%, 90%)',
+        'accent': 'hsl(347, 79%, 50%)',
+      }
     },
-    colors: {
-      'text': 'hsl(197, 78%, 7%)',
-      'background': 'hsl(195, 80%, 98%)',
-      'primary': 'hsl(17, 79%, 68%)',
-      'secondary': 'hsl(47, 80%, 90%)',
-      'accent': 'hsl(347, 79%, 50%)',
-    }
+    
   },
   plugins: []
 }
