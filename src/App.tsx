eed09a2f0283b86e55dadd90e807cf88318aefe8
ray-jpl <72333601+ray-jpl@users.noreply.github.com>
2023-09-03 22:53:40 +1000

    Update background colour

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import WebsitesList from './components/WebsiteList';
 
 function App() {
   return (
-    <div className="w-96 h-96 flex flex-col bg-background text-text font-sans">
+    <div className="w-96 h-96 flex flex-col bg-[#f8f9fa] text-text font-sans">
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
         <PopupHeader/>
         <WebsitesList/>
