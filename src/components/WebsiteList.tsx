30d8f2670d5aae1255ce63ffb2fda131fe27a3cb
ray-jpl <72333601+ray-jpl@users.noreply.github.com>
2023-09-03 22:52:03 +1000

    Fixed additional window event listeners

--- src/components/WebsiteList.tsx
+++ src/components/WebsiteList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SiteInfo } from '../types';
 import SiteMenuItem from './SiteMenuItem';
 
@@ -12,11 +12,18 @@ const WebsitesList: React.FC = () => {
   const [websiteList, setWebsiteList] = useState<WebsiteData>({});
   const [currentUrl, setCurrentUrl] = useState<string>("");
   const port = chrome.runtime.connect({ name: "popup" });
-  window.addEventListener('focus', function () {
-    // Initialise data on load.
-    // Called twice on load for some reason probs some React refresh thing
+
+  // Initial Load
+  useEffect(() => {
     loadData();
-  });
+  },[])
+
+  useEffect(() => {
+    window.addEventListener("focus", loadData);
+    return () => {
+      window.removeEventListener("focus", loadData);
+    };
+  }, []);
 
   // When React refreshes the popup just adds a new listener, taking more CPU
   // Hence we need to check if listeners already exist
@@ -32,6 +39,7 @@ const WebsitesList: React.FC = () => {
   }
 
   function loadData() {
+    console.log("LOADING POPUP")
     chrome.storage.local.get("currentData", function (data) {
       if (data.currentData) {
         setWebsiteList(data.currentData);
@@ -42,33 +50,31 @@ const WebsitesList: React.FC = () => {
   };
 
   return(
-    <div className="flex w-full h-full flex-col bg-inherit">
+    <div className="flex w-full h-full flex-col bg-inherit items-center px-4">
       { websiteList[currentUrl] &&
-        <SiteMenuItem icon={websiteList[currentUrl].icon} url={currentUrl} time={websiteList[currentUrl].time} focus/>
+        <ul className="w-full bg-white rounded-lg border-2 border-zinc-200 mt-4">
+          <SiteMenuItem icon={websiteList[currentUrl].icon} url={currentUrl} time={websiteList[currentUrl].time} focus/>
+        </ul>
+        
       }
-      <div className="overflow-y-scroll">
-        {Object.entries(websiteList).sort(([,a],[,b]) => b.time-a.time).slice(0, 15).map((key) => {
+      <ul className="overflow-y-scroll bg-white rounded-lg border-2 border-zinc-200 my-4">
+        {Object.entries(websiteList).sort(([,a],[,b]) => b.time-a.time).slice(0, MAX_SHOWN_ITEMS).map((key) => {
           return (<SiteMenuItem icon={key[1].icon} url={key[0]} time={key[1].time}/>)
         })}
-        <div className='flex justify-center items-center'>
-          {Object.keys(websiteList).length > MAX_SHOWN_ITEMS &&
-            <button 
-              onClick={() => {
-                chrome.tabs.create({
-                  url: "js/dashboard.html"
-                }); 
-              }}
-              className='px-2 py-1 rounded-full hover:font-bold hover:bg-zinc-200 hover:underline'
-            >
-              Show More
-            </button> 
-          }
-        </div>
-
-      </div>
-
-
-
+        
+        {Object.keys(websiteList).length > MAX_SHOWN_ITEMS &&
+          <button 
+            onClick={() => {
+              chrome.tabs.create({
+                url: "js/dashboard.html"
+              }); 
+            }}
+            className='px-2 py-1 w-full text-accent hover:underline hover:bg-[#f7f9f9]'
+          >
+            Show More
+          </button> 
+        }
+      </ul>
     </div>
   );
 }
