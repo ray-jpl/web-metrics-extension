84a835c1842ecfd8073e1c5e46d02f530d631971
ray-jpl <72333601+ray-jpl@users.noreply.github.com>
2023-09-04 23:58:29 +1000

    Map routes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import BarChartSqIcon from "./icons/BarChartSqIcon";
+import HomeIcon from "./icons/HomeIcon";
+import SettingsIcon from "./icons/SettingsIcon";
+
+const routes = [
+  {
+    label: "Home",
+    icon: HomeIcon,
+    href: "/"
+  },
+  {
+    label: "Usage Limits",
+    icon: BarChartSqIcon,
+    href: "/page"
+  },
+  {
+    label: "Settings",
+    icon: SettingsIcon,
+    href: "/settings"
+  }
+]
+
 
 const Sidebar = () => {
+  let location = useLocation();
+  console.log(location.pathname);
   return (
     <div className="space-y-4 py-4 flex flex-col h-full border-r-2 border-zinc-200">
       <div className='px-3 py-2 flex-1'>
@@ -14,14 +38,18 @@ const Sidebar = () => {
           <h1 className="text-2xl font-bold">Web Metrics</h1>
         </Link>
         <div className='space-y-1'>
-          <ul>
-            <li>
-              <Link to="/js/dashboard.html">Home</Link>
-            </li>
-            <li>
-              <Link to="page" relative="path">Books</Link>
-            </li>
-          </ul>
+          {routes.map((route) => (
+            <Link 
+              to={route.href}
+              key={route.href}
+              className={`text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-zinc-500 hover:bg-zinc-500/10 rounded-lg transition ${location.pathname === route.href ? "text-zinc-500 bg-zinc-500/10" : "text-black"}`}
+            >
+              <div className="flex items-center">
+                <route.icon className="h-5 w-5 mr-3"/>
+                {route.label}
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
