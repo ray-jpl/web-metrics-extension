c59d4aa7b3ae63ea6fed5959592dba6351f6cf76
ray-jpl <72333601+ray-jpl@users.noreply.github.com>
2023-09-04 23:57:52 +1000

    Placeholder for routes

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,10 @@
+
+const NotFound = () => {
+  return (
+    <div className="h-full flex justify-center items-center">
+      <h1 className="text-3xl font-bold">Page not found</h1>
+    </div>
+  )
+}
+
+export default NotFound
---
+++ src/components/SiteDashboard.tsx
@@ -0,0 +1,8 @@
+
+const SiteDashboard = () => {
+  return (
+    <div className="bg-red-500">SiteDashboard</div>
+  )
+}
+
+export default SiteDashboard
