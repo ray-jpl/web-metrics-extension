a4e1018b71cbd528ec991ae018e09f3fad9cad60
ray-jpl <72333601+ray-jpl@users.noreply.github.com>
2023-10-03 23:36:48 +1100

    Fix url redirect

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full border-r-2 border-zinc-200">
       <div className='px-3 py-2 flex-1'>
-        <Link to="/dashboard.html" className='flex items-center pl-3 mb-14'>
+        <Link to="/" className='flex items-center pl-3 mb-14'>
           <div className="relative h-8 w-8 mr-4">
             <img
               src="../logo_icon@96w.png"
